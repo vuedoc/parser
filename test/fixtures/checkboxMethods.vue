90311c80b24f21f92b1b2aaba873ba7cd8a62cdf
indirectlylit <devon@indirectlylit.com>
2017-08-31 13:03:11 -0700

    add test for new `methodsDefaultPrivate` option

---
+++ test/fixtures/checkboxMethods.vue
@@ -0,0 +1,52 @@
+<template>
+  <label>
+    <input :disabled="disabled" type="text" v-model="checkbox">
+    <!-- Default slot -->
+    <slot></slot>
+    <!-- Use this slot to set the checkbox label -->
+    <slot name="label">Unamed checkbox</slot>
+    <!--
+      This
+      is multiline description
+    -->
+    <slot name="multiline">Unamed checkbox</slot>
+    <slot name="undescribed"></slot>
+    <template></template>
+  </label>
+</template>
+
+<script>
+
+/**
+ * A simple checkbox component
+ */
+export default {
+  name: 'checkbox',
+
+  methods: {
+    /**
+     * @private
+     */
+    privateMethod () {},
+
+    /**
+     * @public
+     */
+    publicMethod () {},
+
+    /**
+     * Check the checkbox
+     */
+    defaultMethod () {},
+
+    /**
+     * @protected
+     */
+    protectedMethod () {},
+
+    uncommentedMethod () {}
+  }
+}
+</script>
+
+<style lang="css" scoped></style>
