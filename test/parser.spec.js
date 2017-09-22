3a5b3f2a96dfcf917562e5ba1543fa136243095c
Sébastien Demanou <demsking@gmail.com>
2017-09-22 07:18:00 +0200

    Add more tests for the parser

--- test/parser.spec.js
+++ test/parser.spec.js
@@ -7,7 +7,8 @@ const assert = require('assert')
 
 const template = `
   <div>
-    Hello
+    <input />
+    <slot/>
   </div>
 `
 
@@ -71,6 +72,212 @@ describe('Parser', () => {
 
       assert.equal(parser.ast, null)
       assert.equal(parser.template, template)
+    })
+  })
+
+  describe('walk()', () => {
+    describe('parseTemplate()', () => {
+      it('should successfully emit default slot', (done) => {
+        const filename = './fixtures/checkbox.vue'
+        const defaultMethodVisibility = 'public'
+        const options = {
+          source: { template },
+          filename,
+          defaultMethodVisibility
+        }
+        const parser = new Parser(options)
+
+        parser.walk().on('slot', (slot) => {
+          assert.equal(slot.name, 'default')
+          assert.equal(slot.description, null)
+          done()
+        })
+      })
+
+      it('should successfully emit default slot with description', (done) => {
+        const filename = './fixtures/checkbox.vue'
+        const defaultMethodVisibility = 'public'
+        const template = `
+          <div>
+            <!-- a comment -->
+            <p>Hello</p>
+            <!-- default slot -->
+            <slot/>
+          </div>
+        `
+        const options = {
+          source: { template },
+          filename,
+          defaultMethodVisibility
+        }
+        const parser = new Parser(options)
+
+        parser.walk().on('slot', (slot) => {
+          assert.equal(slot.name, 'default')
+          assert.equal(slot.description, 'default slot')
+          done()
+        })
+      })
+
+      it('should successfully emit component name with only template', (done) => {
+        const filename = './fixtures/checkbox.vue'
+        const defaultMethodVisibility = 'private'
+        const template = `
+          <div>
+            <!-- a comment -->
+            <p>Hello</p>
+          </div>
+        `
+        const options = {
+          source: { template },
+          filename,
+          defaultMethodVisibility
+        }
+        const parser = new Parser(options)
+
+        parser.walk().on('name', (name) => {
+          assert.equal(name, 'checkbox')
+          done()
+        })
+      })
+    })
+
+    describe('extractProperties(property)', () => {
+      it('should successfully emit component name', (done) => {
+        const filename = './fixtures/checkbox.vue'
+        const defaultMethodVisibility = 'private'
+        const script = `
+          export default {
+            name: 'myInput'
+          }
+        `
+        const options = {
+          source: { script },
+          filename,
+          defaultMethodVisibility
+        }
+        const parser = new Parser(options)
+
+        parser.walk().on('name', (name) => {
+          assert.equal(name, 'my-input')
+          done()
+        })
+      })
+
+      it('should successfully emit generic prop', (done) => {
+        const filename = './fixtures/checkbox.vue'
+        const defaultMethodVisibility = 'public'
+        const script = `
+          export default {
+            props: {
+              id: { type: String, default: '$id' }
+            }
+          }
+        `
+        const options = {
+          source: { script },
+          filename,
+          defaultMethodVisibility
+        }
+        const parser = new Parser(options)
+
+        parser.walk().on('props', (prop) => {
+          assert.equal(prop.visibility, 'public')
+          assert.equal(prop.name, 'id')
+          assert.equal(prop.description, null)
+          assert.deepEqual(prop.keywords, [])
+          assert.deepEqual(prop.value, { type: 'String', default: '$id' })
+          done()
+        })
+      })
+
+      it('should successfully emit v-model prop', (done) => {
+        const filename = './fixtures/checkbox.vue'
+        const defaultMethodVisibility = 'public'
+        const script = `
+          export default {
+            props: {
+              /**
+               * @model v-model keyword
+               */
+              value: { type: String }
+            }
+          }
+        `
+        const options = {
+          source: { script },
+          filename,
+          defaultMethodVisibility
+        }
+        const parser = new Parser(options)
+
+        parser.walk().on('props', (prop) => {
+          assert.equal(prop.visibility, 'public')
+          assert.equal(prop.name, 'v-model')
+          assert.equal(prop.description, '')
+          assert.deepEqual(prop.keywords, [ { name: 'model', description: 'v-model keyword' } ])
+          assert.deepEqual(prop.value, { type: 'String' })
+          done()
+        })
+      })
+
+      it('should successfully emit an unknow item', (done) => {
+        const filename = './fixtures/checkbox.vue'
+        const defaultMethodVisibility = 'public'
+        const script = `
+          export default {
+            unknow: {
+              /**
+               * @model v-model keyword
+               */
+              value: { type: String }
+            }
+          }
+        `
+        const options = {
+          source: { script },
+          filename,
+          defaultMethodVisibility
+        }
+        const parser = new Parser(options)
+
+        parser.walk().on('unknow', (prop) => {
+          done()
+        })
+      })
+
+      it('should successfully emit methods', (done) => {
+        const filename = './fixtures/checkbox.vue'
+        const defaultMethodVisibility = 'private'
+        const script = `
+          export default {
+            methods: {
+              getValue: (ctx) => {}
+            }
+          }
+        `
+        const options = {
+          source: { script },
+          filename,
+          defaultMethodVisibility
+        }
+        const parser = new Parser(options)
+
+        parser.walk().on('methods', (prop) => {
+          assert.equal(prop.visibility, 'private')
+          assert.equal(prop.name, 'getValue')
+          assert.equal(prop.description, null)
+          assert.deepEqual(prop.keywords, [])
+          assert.deepEqual(prop.params, [{
+            name: 'ctx',
+            type: 'Identifier',
+            start: 76,
+            end: 79,
+            range: [76,79]
+          }])
+          done()
+        })
+      })
     })
   })
 })
