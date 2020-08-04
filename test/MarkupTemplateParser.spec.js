f6f766d59f09aa91965c776560f87ecfa5b7066e
Sébastien Demanou <demsking@gmail.com>
2020-08-04 07:39:10 +0200

    Fix finding of sibling node for Markup Node

--- test/MarkupTemplateParser.spec.js
+++ test/MarkupTemplateParser.spec.js
@@ -42,4 +42,48 @@ describe('MarkupTemplateParser', () => {
       ]
     }
   })
+
+  ComponentTestCase({
+    name: '#81 - Slot comment not parsed correctly',
+    options: {
+      filecontent: `
+        <template>
+            <div>
+                <div>
+                    <!-- Slot A -->
+                    <slot name="a">
+                        <span>A</span>
+                    </slot>
+                </div>
+                <div>
+                    <!-- Slot B -->
+                    <slot name="b">
+                        <span>B</span>
+                    </slot>
+                </div>
+            </div>
+        </template>
+      `
+    },
+    expected: {
+      slots: [
+        {
+          kind: 'slot',
+          name: 'a',
+          visibility: 'public',
+          description: 'Slot A',
+          keywords: [],
+          props: []
+        },
+        {
+          kind: 'slot',
+          name: 'b',
+          visibility: 'public',
+          description: 'Slot B',
+          keywords: [],
+          props: []
+        }
+      ]
+    }
+  })
 })
