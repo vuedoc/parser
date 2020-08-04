81419be8e65a8afa3a03ff722f5b9fb80535a73e
Sébastien Demanou <demsking@gmail.com>
2020-08-04 15:26:37 +0200

    Fix parsing of data with enabled options.stringify

---
+++ test/DataParser.spec.js
@@ -0,0 +1,61 @@
+/* eslint-disable max-len */
+/* global describe */
+
+const { ComponentTestCase } = require('./lib/TestUtils')
+
+describe('DataParser', () => {
+  ComponentTestCase({
+    name: 'Data with empty value',
+    options: {
+      filecontent: `
+        <script>
+          export default {
+            data: () => ({
+              initialValue: ''
+            })
+          }
+        </script>
+      `
+    },
+    expected: {
+      data: [
+        {
+          description: '',
+          keywords: [],
+          kind: 'data',
+          name: 'initialValue',
+          initial: '',
+          type: 'string',
+          visibility: 'public' }
+      ]
+    }
+  })
+
+  ComponentTestCase({
+    name: 'Data with empty value with stringify',
+    options: {
+      stringify: true,
+      filecontent: `
+        <script>
+          export default {
+            data: () => ({
+              initialValue: ''
+            })
+          }
+        </script>
+      `
+    },
+    expected: {
+      data: [
+        {
+          description: '',
+          keywords: [],
+          kind: 'data',
+          name: 'initialValue',
+          initial: '""',
+          type: 'string',
+          visibility: 'public' }
+      ]
+    }
+  })
+})
